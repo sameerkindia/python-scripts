3357a084eb24d719924228605c018f1b11aa0e31
sameerkindia <sameerkhanaa5@gmail.com>
2025-07-11 08:28:25 +0530

    coffee machine bugs fixed

--- 004-coffee-machine/app.py
+++ 004-coffee-machine/app.py
@@ -3,20 +3,26 @@ import pprint
 
 is_machine_off = False
 
+# Use resources dictionary directly to manage available resources
 water = resources["water"]
 milk = resources["milk"]
 coffee = resources["coffee"]
-money = resources["money"] 
+# This is just a separate variable, we need to update resources["money"]
+money = resources["money"]
 
 
 def total_price():
+    try:
         rupees_10 = int(input('How many 10 rupees notes you have :- ')) * 10
         rupees_20 = int(input('How many 20 rupees notes you have :- ')) * 20
         rupees_50 = int(input('How many 50 rupees notes you have :- ')) * 50
         rupees_100 = int(input('How many 100 rupees notes you have :- ')) * 100
+    except ValueError:
+        print("Please enter a valid number.")
+        return 0
+    total_rupees = rupees_10 + rupees_20 + rupees_50 + rupees_100
+    return total_rupees
 
-        total_rupees = rupees_10 + rupees_20 + rupees_50 + rupees_100
-        return total_rupees
 
 def is_resource_sufficient(order_ingredients, resources):
     for item in order_ingredients:
@@ -34,27 +40,34 @@ while not is_machine_off:
         is_machine_off = True
 
     elif user_input == 'report':
-        print(f"water : {water}, milk : {milk}, coffee : {coffee}, money : {money}")
+        # Print current resource states including money from resources
+        print(
+            f"water : {resources['water']}, milk : {resources['milk']}, coffee : {resources['coffee']}, money : {resources['money']}")
 
     elif user_input in MENU:
         order = MENU[user_input]
-        ingredients = MENU[user_input]["ingredients"]
+        ingredients = order["ingredients"]
         print(f"Pay {order['cost']}")
 
         if is_resource_sufficient(ingredients, resources):
             user_price = total_price()
 
             if user_price >= order["cost"]:
-                print(f"Here is your order {user_input} and your change {user_price - order["cost"]}")
-                money += order["cost"]
-                water -= ingredients["water"]
-                coffee -= ingredients["coffee"]
+                change = user_price - order["cost"]
+                print(
+                    f"Here is your order {user_input} and your change {change}")
 
-                if ingredients["milk"]:
-                 coffee -= ingredients["milk"]
+                # Add the cost to resources["money"]
+                resources["money"] += order["cost"]
+
+                # Subtract the ingredients used
+                resources["water"] -= ingredients["water"]
+                resources["coffee"] -= ingredients["coffee"]
+                if "milk" in ingredients:
+                    resources["milk"] -= ingredients["milk"]
 
             else:
-                print("you don't have enough money")
+                print("You don't have enough money.")
 
     else:
         print("Not a valid input. Try again!")

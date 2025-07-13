6e1986a57c65e323445ba563bc01769c37a2b93a
sameerkindia <sameerkhanaa5@gmail.com>
2025-07-13 13:24:55 +0530

    Refactor turtle drawing functions and add random color generation for random walk

--- 006-turtle-GUI/main.py
+++ 006-turtle-GUI/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen, colormode
 import random
 
 tinu = Turtle()
@@ -27,21 +27,49 @@ tinu = Turtle()
 # draw_dashed_line(15,10,5)
 
 
+# *****Draw Shapes
 
 # tinu.speed(2)
 
-def draw_shape(sides):
-    for i in range(sides):
-        deg = 3 + i
-        # Set random color based on the iteration
-        tinu.color((random.random(), random.random(), random.random()))
-        # tinu.color((round(i / 4), 0.5, 1))  # Change color based on iteration
-        for _ in range(deg):
-            tinu.forward(100)
-            tinu.right(360 / deg)
+# def draw_shape(sides):
+#     for i in range(sides):
+#         deg = 3 + i
+#         # Set random color based on the iteration
+#         tinu.color((random.random(), random.random(), random.random()))
+#         # tinu.color((round(i / 4), 0.5, 1))  # Change color based on iteration
+#         for _ in range(deg):
+#             tinu.forward(100)
+#             tinu.right(360 / deg)
 
-draw_shape(10)
+# draw_shape(10)
 
+# Random Walk
+
+colormode(255)  # Enable RGB color mode
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_rgb = (r, g, b)
+    return random_rgb
+
+
+def random_walk():
+    tinu.speed("fastest")
+    tinu.pensize(5)
+
+    colors = ["red", "blue", "green", "orange", "purple", "yellow", "black"]
+
+    directions = [0, 90, 180, 270]
+
+    for _ in range(200):
+        # tinu.color(random.choice(colors))
+        tinu.color(random_color())
+        tinu.forward(30)
+        tinu.setheading(random.choice(directions))
+
+random_walk()
 
 
 screen = Screen()
